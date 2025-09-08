339b5a66b7a29f41ad0e38eef511d99777c85375
THLT <nhuhuynhnho254@gmail.com>
2025-09-08 11:01:46 +0700

    test route

--- server.js
+++ server.js
@@ -17,4 +17,8 @@ pg.types.setTypeParser(1082, (val) => val);
 
 app.use("/", webRouter);
 
+router.get("/", (req, res) => {
+  res.json({ msg: "Hello từ Vercel, DB chưa bật" });
+});
+
 module.exports = serverless(app);
